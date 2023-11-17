31be653823511ca9f51bf7e349c87fea323eb9e7
SangEon <sup8369@gmail.com>
2023-11-17 13:11:37 +0900

    rem dist

--- src/js/edgescroller.js
+++ src/js/edgescroller.js
@@ -3,16 +3,22 @@
 
 var timer = null;
 
-export function cancelScrollAction () {
+export function cancelScrollAction() {
   clearTimeout(timer);
 }
 
-export function performEdgeScroll(event, container, clientX, clientY, edgeSize) {
+export function performEdgeScroll(
+  event,
+  container,
+  clientX,
+  clientY,
+  edgeSize
+) {
   if (!container || !edgeSize) {
     cancelScrollAction();
     return false;
   }
-  
+
   // NOTE: Much of the information here, with regard to document dimensions,
   // viewport dimensions, and window scrolling is derived from JavaScript.info.
   // I am consuming it here primarily as NOTE TO SELF.
@@ -23,18 +29,18 @@ export function performEdgeScroll(event, container, clientX, clientY, edgeSize) 
   // recalculated on window-resize events (for the most part). I am keeping it
   // all here in the mousemove event handler to remove as many of the moving
   // parts as possible and keep the demo as simple as possible.
-  
+
   // Get the viewport-relative coordinates of the mousemove event.
   var rect = container.getBoundingClientRect();
   var isBody = container === document.body;
-  
+
   var viewportX = clientX - rect.left;
   var viewportY = clientY - rect.top;
   if (isBody) {
     viewportX = clientX;
     viewportY = clientY;
   }
-  
+
   // Get the viewport dimensions.
   var viewportWidth = rect.width;
   var viewportHeight = rect.height;
@@ -42,31 +48,31 @@ export function performEdgeScroll(event, container, clientX, clientY, edgeSize) 
     viewportWidth = document.documentElement.clientWidth;
     viewportHeight = document.documentElement.clientHeight;
   }
-  
+
   // Next, we need to determine if the mouse is within the "edge" of the
   // viewport, which may require scrolling the window. To do this, we need to
   // calculate the boundaries of the edge in the viewport (these coordinates
   // are relative to the viewport grid system).
   var edgeTop = edgeSize;
   var edgeLeft = edgeSize;
-  var edgeBottom = ( viewportHeight - edgeSize );
-  var edgeRight = ( viewportWidth - edgeSize );
-  
-  var isInLeftEdge = ( viewportX < edgeLeft );
-  var isInRightEdge = ( viewportX > edgeRight );
-  var isInTopEdge = ( viewportY < edgeTop );
-  var isInBottomEdge = ( viewportY > edgeBottom );
-  
+  var edgeBottom = viewportHeight - edgeSize;
+  var edgeRight = viewportWidth - edgeSize;
+
+  var isInLeftEdge = viewportX < edgeLeft;
+  var isInRightEdge = viewportX > edgeRight;
+  var isInTopEdge = viewportY < edgeTop;
+  var isInBottomEdge = viewportY > edgeBottom;
+
   // If the mouse is not in the viewport edge, there's no need to calculate
   // anything else.
   if (!(isInLeftEdge || isInRightEdge || isInTopEdge || isInBottomEdge)) {
     cancelScrollAction();
     return false;
   }
-  
+
   // If we made it this far, the user's mouse is located within the edge of the
   // viewport. As such, we need to check to see if scrolling needs to be done.
-  
+
   // Get the document dimensions.
   var documentWidth = Math.max(
     container.scrollWidth,
@@ -78,13 +84,13 @@ export function performEdgeScroll(event, container, clientX, clientY, edgeSize) 
     container.offsetHeight,
     container.clientHeight
   );
-  
+
   // Calculate the maximum scroll offset in each direction. Since you can only
   // scroll the overflow portion of the document, the maximum represents the
   // length of the document that is NOT in the viewport.
-  var maxScrollX = (documentWidth - viewportWidth);
-  var maxScrollY = (documentHeight - viewportHeight);
-  
+  var maxScrollX = documentWidth - viewportWidth;
+  var maxScrollY = documentHeight - viewportHeight;
+
   // As we examine the mousemove event, we want to adjust the window scroll in
   // immediate response to the event; but, we also want to continue adjusting
   // the window scroll if the user rests their mouse in the edge boundary. To
@@ -92,13 +98,13 @@ export function performEdgeScroll(event, container, clientX, clientY, edgeSize) 
   // a timer that continues to invoke the adjustment logic while the window can
   // still be scrolled in a particular direction.
   (function checkForWindowScroll() {
-    cancelScrollAction()
-    
+    cancelScrollAction();
+
     if (adjustWindowScroll()) {
-      timer = setTimeout( checkForWindowScroll, 30 );
+      timer = setTimeout(checkForWindowScroll, 5);
     }
   })();
-  
+
   // Adjust the window scroll based on the user's mouse position. Returns True
   // or False depending on whether or not the window scroll was changed.
   function adjustWindowScroll() {
@@ -109,67 +115,63 @@ export function performEdgeScroll(event, container, clientX, clientY, edgeSize) 
       currentScrollX = window.pageXOffset;
       currentScrollY = window.pageYOffset;
     }
-    
+
     // Determine if the window can be scrolled in any particular direction.
-    var canScrollUp = (currentScrollY > 0);
-    var canScrollDown = (currentScrollY < maxScrollY);
-    var canScrollLeft = (currentScrollX > 0);
-    var canScrollRight = (currentScrollX < maxScrollX);
-    
+    var canScrollUp = currentScrollY > 0;
+    var canScrollDown = currentScrollY < maxScrollY;
+    var canScrollLeft = currentScrollX > 0;
+    var canScrollRight = currentScrollX < maxScrollX;
+
     // Since we can potentially scroll in two directions at the same time,
     // let's keep track of the next scroll, starting with the current scroll.
     // Each of these values can then be adjusted independently in the logic
     // below.
     var nextScrollX = currentScrollX;
     var nextScrollY = currentScrollY;
-    
+
     // As we examine the mouse position within the edge, we want to make the
     // incremental scroll changes more "intense" the closer that the user
     // gets the viewport edge. As such, we'll calculate the percentage that
     // the user has made it "through the edge" when calculating the delta.
     // Then, that use that percentage to back-off from the "max" step value.
-    var maxStep = 50;
-    
+    var maxStep = 10;
+
     // Should we scroll left?
     if (isInLeftEdge && canScrollLeft) {
-      var intensity = ((edgeLeft - viewportX) / edgeSize);
-      nextScrollX = (nextScrollX - (maxStep * intensity));
+      var intensity = (edgeLeft - viewportX) / edgeSize;
+      nextScrollX = nextScrollX - maxStep * intensity;
     }
     // Should we scroll right?
     else if (isInRightEdge && canScrollRight) {
-      var intensity = ((viewportX - edgeRight) / edgeSize);
-      nextScrollX = (nextScrollX + (maxStep * intensity));
+      var intensity = (viewportX - edgeRight) / edgeSize;
+      nextScrollX = nextScrollX + maxStep * intensity;
     }
-    
+
     // Should we scroll up?
     if (isInTopEdge && canScrollUp) {
-      var intensity = ((edgeTop - viewportY) / edgeSize);
-      nextScrollY = (nextScrollY - (maxStep * intensity));
+      var intensity = (edgeTop - viewportY) / edgeSize;
+      nextScrollY = nextScrollY - maxStep * intensity;
     }
     // Should we scroll down?
     else if (isInBottomEdge && canScrollDown) {
-      var intensity = ((viewportY - edgeBottom) / edgeSize);
-      nextScrollY = (nextScrollY + (maxStep * intensity));
+      var intensity = (viewportY - edgeBottom) / edgeSize;
+      nextScrollY = nextScrollY + maxStep * intensity;
     }
-    
+
     // Sanitize invalid maximums. An invalid scroll offset won't break the
     // subsequent .scrollTo() call; however, it will make it harder to
     // determine if the .scrollTo() method should have been called in the
     // first place.
     nextScrollX = Math.max(0, Math.min(maxScrollX, nextScrollX));
     nextScrollY = Math.max(0, Math.min(maxScrollY, nextScrollY));
-    
-    if (
-      (nextScrollX !== currentScrollX) ||
-      (nextScrollY !== currentScrollY)
-    ) {
+
+    if (nextScrollX !== currentScrollX || nextScrollY !== currentScrollY) {
       (isBody ? window : container).scrollTo(nextScrollX, nextScrollY);
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   }
-  
-  return true
+
+  return true;
 }
